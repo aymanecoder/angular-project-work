a01319a7e84f57a130c26416fa388681ffb62059
aymanecoder <aymanbiti2@gmail.com>
2023-06-11 00:40:31 +0100

    delete api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,5 @@
 export const environement = {
   production:false,
-  // apiUrl: 'http://my-prod-url'
-  // apiUrl : "https://moviesdatabase.p.rapidapi.com"
-
   apiUrl: "http://localhost:8082/api/v1"
 
 }
