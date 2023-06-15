c631b49adc0951002505831371e736ad1ac7cf8b
aymanecoder <aymanbiti2@gmail.com>
2023-06-15 16:21:49 +0100

    service

--- src/app/_core/services/suivie/suivie.service.ts
+++ src/app/_core/services/suivie/suivie.service.ts
@@ -1,9 +1,75 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environement } from 'src/environments/environment';
+import { Suivie } from '../../interfaces/Suivie';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SuivieService {
 
-  constructor() { }
+  apiUrl=environement.apiUrl ;
+  constructor(private http:HttpClient) { }
+
+  getSuivies():Observable<Suivie[]> {
+    const token = localStorage.getItem('access_token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`)
+      return this.http.get<Suivie[]>(`${this.apiUrl}/manager/suivies`,{headers});
+
+  }
+
+
+
+
+  addSuivie(suivie: Suivie): Observable<Suivie> {
+    const token = localStorage.getItem('access_token'); // Check for JWT in local storage
+
+    if (!token) {
+      throw new Error('Authorization token not found');
+    }
+
+
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      })
+
+
+    return this.http.post<Suivie>(`${this.apiUrl}/manager/suivie`, suivie,{headers: headers});
+  }
+
+  updateSuivie(suivie: Suivie): Observable<Suivie> {
+    const token = localStorage.getItem('access_token'); // Check for JWT in local storage
+
+    if (!token) {
+      throw new Error('Authorization token not found');
+    }
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    });
+
+    const url = `${this.apiUrl}/manager/suivie/${suivie.id}`; // Use the ID of the appliance to update
+
+    return this.http.put<Suivie>(url, suivie, { headers });
+  }
+
+
+
+  deleteSuivie(id: number) {
+    const token = localStorage.getItem('access_token'); // Check for JWT in local storage
+
+    if (!token) {
+      throw new Error('Authorization token not found');
+    }
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    });
+    const url = `${this.apiUrl}/manager/suivie/${id}`;
+    return this.http.delete(url,{headers: headers});
+  }
 }
